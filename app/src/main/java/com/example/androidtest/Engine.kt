44543004e597d08cf93a67f6cbcfe9e8826f6fc8
Özgür Can <sezginzgurr@gmail.com>
2022-01-19 16:56:03 +0300

    Added basic test

---
+++ app/src/main/java/com/example/androidtest/Engine.kt
@@ -0,0 +1,18 @@
+package com.example.androidtest
+
+data class Engine(
+    val cc: Int,
+    val horsePower: Int,
+    var temperature: Int,
+    var isTurnedOn: Boolean
+) {
+    fun turnOn(){
+        isTurnedOn = true
+        temperature = 95
+    }
+
+    fun turnOff(){
+        isTurnedOn = false
+        temperature = 15
+    }
+}
---
+++ app/src/test/java/com/example/androidtest/EngineTest.kt
@@ -0,0 +1,24 @@
+package com.example.androidtest
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EngineTest {
+
+    private val engine = Engine(2000, 189, 15, false)
+
+    @Test
+    fun engineTurnsOn() {
+        engine.turnOn()
+        assertEquals(true, engine.isTurnedOn)
+        assertEquals(95, engine.temperature)
+    }
+
+    @Test
+    fun engineTurnsOff() {
+        engine.turnOff()
+
+        assertEquals(false, engine.isTurnedOn)
+        assertEquals(15, engine.temperature)
+    }
+}
